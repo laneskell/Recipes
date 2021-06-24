d9f853ec72d2e97995c885c842cd91cdc4dd927c
laneskell <laneskell@gmail.com>
2021-06-24 13:40:26 -0300

    storyboard-first-screen

---
+++ suduRecipes/suduRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  suduRecipes
+//
+//  Created by Kell Lanes on 24/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+
+}
+
